e0b6c90578fc78407ea08c01fea82bd5704be40a
+ <+>
2023-03-01 23:19:34 -0500

    commit tree 0b63e2fef4564dadf51fe654b0b17983137b784b

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,27 @@
 #write your code here
 
-def countdown
-end
+# def countdown (seconds)
+#     i = seconds
+# while i > 0 
+#     puts "#{i} SECOND(S)!"
+#     i -= 1   
+# end
+
+# end
+
+def countdown(seconds_to_midnight)
+    while seconds_to_midnight > 0
+      puts "#{seconds_to_midnight} SECOND(S)!"
+      seconds_to_midnight -= 1
+    end
+    "HAPPY NEW YEAR!"
+  end
+
+  def countdown_with_sleep(seconds_to_midnight)
+    while seconds_to_midnight > 0
+      puts "#{seconds_to_midnight} SECOND(S)!"
+      sleep(1)
+      seconds_to_midnight -= 1
+    end
+    "HAPPY NEW YEAR!"
+  end
